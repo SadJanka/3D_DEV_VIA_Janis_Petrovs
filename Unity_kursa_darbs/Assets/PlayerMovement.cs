6b127f53152570001e90ea00a0d32d4a09b07ac5
SadJanka <janispetrovs2013.jp@gmail.com>
2026-05-15 15:21:26 +0300

    Add layer capsule with script

--- Unity_kursa_darbs/Assets/PlayerMovement.cs
+++ Unity_kursa_darbs/Assets/PlayerMovement.cs
@@ -1,6 +1,47 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovement
+public class PlayerMovement : MonoBehaviour
 {
-    
+    public CharacterController controller;
+    public float speed = 5f;
+    public float gravity = -9.81f;
+    public float sensitivity = 2f;
+
+    Vector3 velocity;
+    float rotationX = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // 1. Skatīšanās apkārt
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.Rotate(Vector3.up * mouseX);
+
+        // 2. Kustība uz priekšu/sāniem
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // 3. Gravitācija
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // "Piespiež" pie zemes
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
 }
